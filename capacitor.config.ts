7451ad893c82bef2b5512d4557dd6aeccb23d575
Alice Aliaga <alicealiagamoreno@gmail.com>
2025-05-10 11:55:36 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ICC0008-UF3-PR01-AliceAliagaMoreno',
+  webDir: 'www'
+};
+
+export default config;
